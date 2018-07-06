0c80458a84114fe91dd1ddef1a293b281eb56890
scherrma <marty@Sandalphon.localdomain>
2018-07-05 23:48:30 -0400

    new grid.py done

--- 2d/grid.py
+++ 2d/grid.py
@@ -3,34 +3,47 @@ from random import choice
 from copy import deepcopy
 
 class Fourgrid:
-    def __init__(self, hsize):
-        self.hsize = hsize
-        self.size = 2*hsize
-        self.data = [[choice([True, False]) for i in range(self.hsize)] for j in range(self.hsize)]
+    def __init__(self, size):
+        self.size = size
+        self.hsize = (size-1)//2
+        self.odd = (size % 2 == 1)
+        self.data = [choice([True, False]) for i in range(size*size//4 + size%2)]
 
-    def at(self, x, y):
-        x %= self.size
-        y %= self.size
-        if(y >= self.hsize):
+    def __loc(self, coords):
+        x = coords[0] % self.size
+        y = coords[1] % self.size
+
+        if self.odd and x == self.hsize and y == self.hsize:
+            return len(self.data)-1
+        
+        elif x > self.hsize or (x == self.hsize and y > self.hsize):
+            x = self.size - x - 1
             y = self.size - y - 1
-            x = self.size - x - 1
-        if(x >= self.hsize):
-            x = self.size - x - 1
-            x, y = y, x
-        return self.data[y][x]
+        if y >= self.hsize:
+            y, x = x, self.size - y - 1
+        return x + y*(self.hsize + int(self.odd))
+
+    def __getitem__(self, coords):
+        return self.data[self.__loc(coords)]
+
+    def __setitem__(self, coords, value):
+        self.data[self.__loc(coords)] = value
 
     def __str__(self):
         lines = []
-        for i in range(-self.hsize, self.size+self.hsize):
-            lines.append(''.join([colored(2*u"\u2588", "cyan" if self.at(j, i) else "yellow") for j in range(-self.hsize,self.size+self.hsize)]))
+        for y in range(self.size):
+            lines.append(''.join([colored(2*u"\u2588", "cyan" if self[x, y] else "yellow") for x in range(self.size)]))
+        #for y in range(self.size):
+        #    lines.append(' '.join([str(self.__loc((x,y))) for x in range(self.size)]))
         return '\n'.join(lines)
-
+        self.data = newdata
+        
     def cool(self):
         newdata = deepcopy(self.data)
         for x in range(self.hsize):
             for y in range(self.hsize):
-                nbors = sum([int(self.at(x+i, y+j)) for i in (-1, 0, 1) for j in (-1, 0, 1)])
-                newdata[y][x] = (nbors > 4)
+                nbors = sum([int(self[x+i, y+j]) for i in (-1, 0, 1) for j in (-1, 0, 1)])
+                newdata[self.__loc((x, y))] = (nbors > 4)
         self.data = newdata
 
     def altcool(self):
@@ -38,6 +51,6 @@ class Fourgrid:
         newdata = deepcopy(self.data)
         for x in range(self.hsize):
             for y in range(self.hsize):
-                nbors = sum([(coolrange**2 > i**2 + j**2 if int(self.at(x+i, y+j)) else 0) for i in range(-coolrange, coolrange+1) for j in range(-coolrange, coolrange+1)])
-                newdata[y][x] = (nbors > 4)
+                nbors = sum([(coolrange**2 > i**2 + j**2 if int(self[x+i, y+j]) else 0) for i in range(-coolrange, coolrange+1) for j in range(-coolrange, coolrange+1)])
+                newdata[self.__loc((x, y))] = (nbors > 4)
         self.data = newdata
--- 2d/test2.py
+++
@@ -1,9 +0,0 @@
-from grid2 import Fourgrid
-from termcolor import colored
-
-a = Fourgrid(7)
-print("a.size:",a.size,"\ta.hsize:",a.hsize)
-print("a[0,0]:",a[0,0])
-print("setting a[0,0] to true")
-a[0,0] = True
-print("a[0,0]:",a[0,0])
